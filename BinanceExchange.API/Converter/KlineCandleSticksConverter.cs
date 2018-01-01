00175d719a32419e075352225548a0875995cb4f
Stefan Kelemen <photosynthese@gmx.at>
2018-01-02 00:26:49 +0100

    corrected kline json writer to make arrays with seperator

--- BinanceExchange.API/Converter/KlineCandleSticksConverter.cs
+++ BinanceExchange.API/Converter/KlineCandleSticksConverter.cs
@@ -13,21 +13,27 @@ namespace BinanceExchange.API.Converter
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
             var candle = value as KlineCandleStickResponse;
-            var jarr = new JArray() {
-                (long)candle.OpenTime.Subtract(Epoch).TotalMilliseconds,
-                candle.Open,
-                candle.High,
-                candle.Low,
-                candle.Close,
-                candle.Volume,
-                (long)candle.CloseTime.Subtract(Epoch).TotalMilliseconds,
-                candle.QuoteAssetVolume,
-                candle.NumberOfTrades,
-                candle.TakerBuyBaseAssetVolume,
-                candle.TakerBuyQuoteAssetVolume,
-            };
-            writer.WriteRaw(jarr.ToString());
+            //var jarr = new JArray() {
+            writer.WriteStartArray();
 
+            writer.WriteValue((long)candle.OpenTime.Subtract(Epoch).TotalMilliseconds);
+            writer.WriteValue(candle.Open);
+            writer.WriteValue(candle.High);
+            writer.WriteValue(candle.Low);
+            writer.WriteValue(candle.Close);
+            writer.WriteValue(candle.Volume);
+            writer.WriteValue((long)candle.CloseTime.Subtract(Epoch).TotalMilliseconds);
+            writer.WriteValue(candle.QuoteAssetVolume);
+            writer.WriteValue(candle.NumberOfTrades);
+            writer.WriteValue(candle.TakerBuyBaseAssetVolume);
+            writer.WriteValue(candle.TakerBuyQuoteAssetVolume);
+            //};
+            
+
+            //foreach (var val in jarr)
+            //val
+
+            writer.WriteEndArray();
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
@@ -36,17 +42,17 @@ namespace BinanceExchange.API.Converter
 
             return new KlineCandleStickResponse
             {
-                OpenTime = Epoch.AddMilliseconds((long) klineCandlesticks.ElementAt(0)),
-                Open = (decimal) klineCandlesticks.ElementAt(1),
-                High = (decimal) klineCandlesticks.ElementAt(2),
-                Low = (decimal) klineCandlesticks.ElementAt(3),
-                Close = (decimal) klineCandlesticks.ElementAt(4),
-                Volume = (decimal) klineCandlesticks.ElementAt(5),
-                CloseTime = Epoch.AddMilliseconds((long) klineCandlesticks.ElementAt(6)),
-                QuoteAssetVolume = (decimal) klineCandlesticks.ElementAt(7),
-                NumberOfTrades = (int) klineCandlesticks.ElementAt(8),
-                TakerBuyBaseAssetVolume = (decimal) klineCandlesticks.ElementAt(9),
-                TakerBuyQuoteAssetVolume = (decimal) klineCandlesticks.ElementAt(10),
+                OpenTime = Epoch.AddMilliseconds((long)klineCandlesticks.ElementAt(0)),
+                Open = (decimal)klineCandlesticks.ElementAt(1),
+                High = (decimal)klineCandlesticks.ElementAt(2),
+                Low = (decimal)klineCandlesticks.ElementAt(3),
+                Close = (decimal)klineCandlesticks.ElementAt(4),
+                Volume = (decimal)klineCandlesticks.ElementAt(5),
+                CloseTime = Epoch.AddMilliseconds((long)klineCandlesticks.ElementAt(6)),
+                QuoteAssetVolume = (decimal)klineCandlesticks.ElementAt(7),
+                NumberOfTrades = (int)klineCandlesticks.ElementAt(8),
+                TakerBuyBaseAssetVolume = (decimal)klineCandlesticks.ElementAt(9),
+                TakerBuyQuoteAssetVolume = (decimal)klineCandlesticks.ElementAt(10),
             };
         }
 
